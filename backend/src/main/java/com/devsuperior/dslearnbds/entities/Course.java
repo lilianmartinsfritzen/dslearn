77409ea377dced86af0f85e50f885f281fef64c3
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-11-06 15:34:28 -0300

    feat(BACKEND): implemented association between course and offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_course")
@@ -14,6 +16,8 @@ public class Course implements Serializable {
     private String name;
     private String imgUri;
     private String imgGrayUri;
+
+    private List<Offer> offers = new ArrayList<>();
 
     public Course() {}
 
@@ -54,6 +58,10 @@ public class Course implements Serializable {
 
     public void setImgGrayUri(String imgGrayUri) {
         this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
